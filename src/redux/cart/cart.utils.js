745ea518ee54c0328ca7258f15373bbc63443236
Daniel Fagonde <cdfagonde@hotmail.com>
2020-11-27 20:20:06 -0300

    Lesson 136 done: created addItem and removeItem to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,4 +12,26 @@ export const addItemToCart = ( cartItems, itemToAdd ) => {
 
     // Se o item não existia na sacola..
     return [...cartItems, { ...itemToAdd, quantity: 1 }]
+};
+
+
+export const removeItemFromCart = ( cartItems, itemToRemove ) => {
+    const existingCartItems = cartItems.find( item => item.id === itemToRemove.id );
+
+    if (existingCartItems.quantity === 1) {
+        // Só temos 1, então excluimos o item
+        return cartItems.filter( item => item.id !== itemToRemove.id );
+    }
+
+    if (existingCartItems.quantity > 1) {
+        // Temos vários, então diminuimos a quantidade
+        return cartItems.map( item => 
+            item.id === itemToRemove.id
+                ? { ...item, quantity: item.quantity-1 }
+                : item
+        );
+    } else {
+        // Não deveria ocorrer, mas neste caso fica tudo como está
+        return cartItems;
+    }
 }
