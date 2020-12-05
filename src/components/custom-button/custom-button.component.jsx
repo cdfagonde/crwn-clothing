5c225f8e621e21b271d9d651dceae892aa2f0a66
Daniel Fagonde <cdfagonde@hotmail.com>
2020-12-05 20:10:22 -0300

    Lesson 167 done: Converted CustomButton to styled-components, but decided not to use it

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
+/*
+  Versão inicial usando scss.
+*/
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSingin, inverted, ...otherProps}) => (
     <button 
-        className={`${inverted && 'inverted'} ${isGoogleSingin && 'google-sign-in'} custom-button`}
+        className={`${inverted ? 'inverted' : ''} ${isGoogleSingin ? 'google-sign-in' : '' } custom-button`}
         {...otherProps}>
         {children}
     </button>
 );
+
+/*
+  Versão usando styles-components.
+*/
+
+// import { CustomButtonContainer } from './custom-button.styles';
+
+// const CustomButton = ({children, ...props}) => (
+//     <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
 
 export default CustomButton;
