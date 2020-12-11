2a327103b6ad66e14f69514e4408fd23bf7afdea
Daniel Fagonde <cdfagonde@hotmail.com>
2020-12-11 19:58:58 -0300

    Less 174 done: Modified collection-preview comp for not using routename of shop-data

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,10 +6,10 @@ import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 
-const CollectionPreview = ({ title, items, history, match, routeName }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
     <div className='collection-preview'>
         <h1 className='title' 
-            onClick={() => history.push(`${match.url}/${routeName}`) }>{title.toUpperCase()}</h1>
+            onClick={() => history.push(`${match.url}/${title.toLowerCase()}`) }>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 items
