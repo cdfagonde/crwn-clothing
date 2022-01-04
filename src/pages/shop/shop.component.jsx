08bb8c707da0578dc258629a386e5fa60a5dab0f
Daniel Fagonde <cdfagonde@hotmail.com>
2022-01-04 20:07:55 -0300

    Primeira implementação de Reduc-Saga no componente Shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ import CollectionOverviewContainer from '../../components/collections-overview/c
 
 import CollectionPageContainer from '../collection/collection.container';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 // import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 // import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
@@ -69,8 +69,8 @@ class ShopPage extends React.Component {
 
     // 
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
     }
 
     // 
@@ -92,7 +92,7 @@ class ShopPage extends React.Component {
 
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null,mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 // Somente queremos logger durante o desenvolvimento
 if (process.env.NODE_ENV === 'development') {
@@ -14,6 +18,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export const store = createStore( rootReducer, applyMiddleware( ...middlewares ));
+
+sagaMiddleware.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { store, persistor };
